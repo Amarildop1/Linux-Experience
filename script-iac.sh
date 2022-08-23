2cc8d2c410e4b65feaa4fbdbaa27091aba6a132f
Amarildo <amarildop1dev@gmail.com>
2022-08-22 23:20:53 -0300

    Correção de erro do -crypt | funcionou ao trocar por -1 no parâmetro do openssl

--- script-iac.sh
+++ script-iac.sh
@@ -18,17 +18,17 @@ echo "\n ------------------------------------- \n"
 
 echo "Criando usuários..."
 
-useradd jhon -c "Jhon" -s /bin/bash -m -p $(openssl passwd -crypt senhajhon) -G GRUPO-ADM
-useradd paul -c "Paul" -s /bin/bash -m -p $(openssl passwd -crypt senhapaul) -G GRUPO-ADM
-useradd alen -c "Alen" -s /bin/bash -m -p $(openssl passwd -crypt senhaalen) -G GRUPO-ADM
+useradd jhon -c "Jhon" -s /bin/bash -m -p $(openssl passwd -1 senhajhon) -G GRUPO-ADM
+useradd paul -c "Paul" -s /bin/bash -m -p $(openssl passwd -1 senhapaul) -G GRUPO-ADM
+useradd alen -c "Alen" -s /bin/bash -m -p $(openssl passwd -1 senhaalen) -G GRUPO-ADM
 
-useradd debora -c "Debora" -s /bin/bash -m -p $(openssl passwd -crypt senhabedora) -G GRUPO-VEN
-useradd sebastiana -c "Sebastiana" -s /bin/bash -m -p $(openssl passwd -crypt senhasebastiana) -G GRUPO-VEN
-useradd roberto -c "Roberto" -s /bin/bash -m -p $(openssl passwd -crypt senharoberto) -G GRUPO-VEN
+useradd debora -c "Debora" -s /bin/bash -m -p $(openssl passwd -1 senhabedora) -G GRUPO-VEN
+useradd sebastiana -c "Sebastiana" -s /bin/bash -m -p $(openssl passwd -1 senhasebastiana) -G GRUPO-VEN
+useradd roberto -c "Roberto" -s /bin/bash -m -p $(openssl passwd -1 senharoberto) -G GRUPO-VEN
 
-useradd josefina -c "Josefina" -s /bin/bash -m -p $(openssl passwd -crypt senhajosefina) -G GRUPO-SEC
-useradd amanda -c "Amanda" -s /bin/bash -m -p $(openssl passwd -crypt senhaamanda) -G GRUPO-SEC
-useradd joana -c "Joana" -s /bin/bash -m -p $(openssl passwd -crypt senhajoana) -G GRUPO-SEC
+useradd josefina -c "Josefina" -s /bin/bash -m -p $(openssl passwd -1 senhajosefina) -G GRUPO-SEC
+useradd amanda -c "Amanda" -s /bin/bash -m -p $(openssl passwd -1 senhaamanda) -G GRUPO-SEC
+useradd joana -c "Joana" -s /bin/bash -m -p $(openssl passwd -1 senhajoana) -G GRUPO-SEC
 
 echo "Usuários criados com sucesso!"
 echo "\n ------------------------------------- \n"
